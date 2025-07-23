7138b839b3723790ac2584068d4b18397c941d04
ARULSELVAN M <153312802+arulselvan02@users.noreply.github.com>
2025-07-23 14:42:02 +0530

    Time: 0 ms (100%), Space: 43 MB (85.87%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] intersect(int[] n1, int[] n2) {
+        int a[] = new int[1001];
+        int[] max, min;
+
+        if(n1.length < n2.length) {
+            max = n2;
+            min = n1;
+        } else {
+            max = n1;
+            min = n2;
+        }
+
+        for (int i : max)
+            a[i]++;
+
+        int[] result = new int[min.length];
+        int c = 0;
+
+        for (int i : min) {
+            if (a[i] > 0) {
+                result[c++] = i;
+                a[i]--;  
+            }
+        }
+
+        
+        return java.util.Arrays.copyOfRange(result, 0, c);
+    }
+}
