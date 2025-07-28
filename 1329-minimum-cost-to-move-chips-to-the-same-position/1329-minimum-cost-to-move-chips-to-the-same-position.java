560526fef12c4682bda42d9632641367474af30c
ARULSELVAN M <153312802+arulselvan02@users.noreply.github.com>
2025-07-28 14:28:18 +0530

    Time: 0 ms (100%), Space: 41.1 MB (70.2%) - LeetHub

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/1329-minimum-cost-to-move-chips-to-the-same-position.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int o = 0;
+        int e = 0;
+        for(int p:position){
+            if(p%2==0) e++;
+            else o++;
+        }
+        return Math.min(o,e);
+    }
+}
