f9f6122e58c26a72d37f17a5fdceb37937b5a709
ARULSELVAN M <153312802+arulselvan02@users.noreply.github.com>
2025-08-15 10:55:20 +0530

    Time: 0 ms (100%), Space: 40.6 MB (89.5%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0) return false;
+
+        if((n&(n-1)) == 0 && Integer.bitCount(n-1)%2==0) return true;
+
+        return false;
+    }
+}
